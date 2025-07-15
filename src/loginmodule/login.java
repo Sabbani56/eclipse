d3f8d130a1635fe0da01ee606d83c0018cbb5b81
Sabbani56 <thirupathisabbaniqa@gmail.com>
2025-07-15 22:35:07 +0530

    add new

---
+++ src/loginmodule/login.java
@@ -0,0 +1,13 @@
+package loginmodule;
+
+public class login {
+
+	public static void main(String[] args) {
+		
+		System.out.println( "usrename");
+		
+		System.out.println("passward");
+		
+	}
+}
+	
